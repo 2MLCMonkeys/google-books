f6a370ee2f7f07526152e443395bf448dbf70e84
2mlcmonkeys <2mlcmonkeys@gmail.com>
2020-05-04 15:11:23 -0400

    adding server function and book models

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+// DEPENDENCIES //
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+// SET PORT //
 const PORT = process.env.PORT || 3001;
+
+// INTIALIZE SERVER //
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+const routes = require("./routes/routes");
+
+// ADD API ROUTES //
+app.use(routes);
+
+// MONGODB CONNECTION //
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Send every other request to the React app
 // Define any API routes before this runs
